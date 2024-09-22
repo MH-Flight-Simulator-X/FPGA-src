081130cad9052ae724d34cac4ae1ea7aef1833c1
Andreas V. Jonsterhaug <andreas.vj@icloud.com>
2024-09-22 22:20:14 +0200

    renamed frammebuffer variables

--- lib/Framebuffer/src/framebuffer.sv
+++ lib/Framebuffer/src/framebuffer.sv
@@ -1,13 +1,13 @@
 // Framebuffer for a display of custom size
 
-`timescale 1ps / 1ps
+`timescale 1ns / 1ps
 
 module framebuffer #(
-    parameter SCREEN_WIDTH,
-    parameter SCREEN_HEIGHT,
-    parameter INDEX_WIDTH,
-    localparam BUFFER_SIZE = SCREEN_WIDTH*SCREEN_HEIGHT,
-    localparam ADDR_WIDTH = $clog2(BUFFER_SIZE)
+    parameter FB_WIDTH,
+    parameter FB_HEIGHT,
+    parameter DATA_WIDTH,
+    localparam FB_SIZE = FB_WIDTH*FB_HEIGHT,
+    localparam ADDR_WIDTH = $clog2(FB_SIZE)
     ) (
     input wire clk_write,
     input wire clk_read,
@@ -15,16 +15,16 @@ module framebuffer #(
     input wire rst,
     input wire [ADDR_WIDTH-1:0] addr_write,
     input wire [ADDR_WIDTH-1:0] addr_read,
-    input wire [INDEX_WIDTH-1:0] data_in, 
-    output reg [INDEX_WIDTH-1:0] data_out
+    input wire [DATA_WIDTH-1:0] data_in, 
+    output reg [DATA_WIDTH-1:0] data_out
     );
 
-    logic [INDEX_WIDTH-1:0] buffer [0:BUFFER_SIZE-1]; 
+    logic [DATA_WIDTH-1:0] buffer [0:FB_SIZE-1];
 
     always_ff @(posedge clk_write or posedge rst) begin
         if (rst) begin
             integer i;
-            for (i = 0; i < BUFFER_SIZE; i = i + 1) begin
+            for (i = 0; i < FB_SIZE; i = i + 1) begin
                 buffer[i] <= '0;
             end
         end else if (write_enable) begin
@@ -37,3 +37,4 @@ module framebuffer #(
     end
 
 endmodule
+
