c77da2f091f2ce00c22bff6631dce7f0187d4a0e
Morten Sørensen <morten.sorensen.04@gmail.com>
2024-10-07 21:40:33 +0200

    working mat mul with configurable frac bits

---
+++ lib/Math/MatMul/src/mat_mul.sv
@@ -0,0 +1,177 @@
+// Module for multiplying a 4x4 matrix by a 4-dim vector of fixed-point data
+// Currently configured for Q10.8
+// Version: 0.1
+
+`timescale 1ns / 1ps
+
+typedef enum logic [1:0] {
+    IDLE = 2'b00,
+    PROCESSING = 2'b01,
+    DONE = 2'b10
+} mat_mat_mul_state_t /*verilator public*/;
+
+module mat_mul #(
+    parameter unsigned DATAWIDTH = 24,
+    parameter unsigned FRACBITS = 13
+    ) (
+    input clk,
+    input rstn,
+
+    // Input Data
+    input signed [DATAWIDTH-1:0] A [4][4],   // 1. Input matrix
+    input signed [DATAWIDTH-1:0] B [4][4],   // 2. Input matrix
+    input logic i_dv,
+
+    // Output Data
+    output logic signed [DATAWIDTH-1:0] C [4][4],  // Output matrix
+    output logic o_dv,
+    output logic o_ready
+    );
+
+    localparam unsigned OutputRangeStart = FRACBITS;
+    localparam unsigned OutputRangeEnd = DATAWIDTH + OutputRangeStart - 1;
+
+    reg [1:0] index = 2'b00;
+    reg i_dv_r;
+    reg o_dv_r;
+    reg o_ready_r;
+    reg signed [DATAWIDTH-1:0] A_r [4][4];
+    reg signed [DATAWIDTH-1:0] B_r [4][4];
+    reg signed [2 * DATAWIDTH-1:0] C_r [4][4];      // Double data width to account for precision
+                                                    // loss. Could be worth experimenting
+                                                    // with only using DATAWIDTH for
+                                                    // intermediate calculation
+
+    mat_mat_mul_state_t current_state = IDLE, next_state = IDLE;
+
+    always_ff @(posedge clk) begin
+        if (~rstn) begin
+            foreach (A_r[i,j]) A_r[i][j] <= '0;
+            foreach (B_r[i,j]) B_r[i][j] <= '0;
+
+            i_dv_r <= 1'b0;
+            current_state <= IDLE;
+
+        end else begin
+            current_state <= next_state;
+
+            if (i_dv && o_ready) begin
+                // Cssign input data to registers
+                A_r[0] <= A[0];
+                A_r[1] <= A[1];
+                A_r[2] <= A[2];
+                A_r[3] <= A[3];
+
+                B_r[0] <= B[0];
+                B_r[1] <= B[1];
+                B_r[2] <= B[2];
+                B_r[3] <= B[3];
+
+            end
+
+            i_dv_r <= i_dv;
+        end
+    end
+
+    // State machine
+    always_comb begin
+        next_state = current_state;
+
+        case (current_state)
+            IDLE: begin
+                if (i_dv_r == 1'b1) begin
+                    next_state = PROCESSING;
+                end
+            end
+
+            PROCESSING: begin
+                if (index == 2'b11) begin
+                    next_state = DONE;
+                end
+            end
+
+            DONE: begin
+                next_state = IDLE;
+            end
+
+            default: next_state = IDLE;
+        endcase
+    end
+
+    // Compute the matrix vector product
+    always_ff @(posedge clk) begin
+        if (~rstn) begin
+            index <= 2'b00;
+            foreach (C_r[i,j]) C_r[i][j] <= '0;
+        end else if (current_state == PROCESSING) begin
+            // DO COMPUTATION
+            C_r[0][0] <= C_r[0][0] + (A_r[0][index] * B_r[index][0]);
+            C_r[0][1] <= C_r[0][1] + (A_r[0][index] * B_r[index][1]);
+            C_r[0][2] <= C_r[0][2] + (A_r[0][index] * B_r[index][2]);
+            C_r[0][3] <= C_r[0][3] + (A_r[0][index] * B_r[index][3]);
+
+            C_r[1][0] <= C_r[1][0] + (A_r[1][index] * B_r[index][0]);
+            C_r[1][1] <= C_r[1][1] + (A_r[1][index] * B_r[index][1]);
+            C_r[1][2] <= C_r[1][2] + (A_r[1][index] * B_r[index][2]);
+            C_r[1][3] <= C_r[1][3] + (A_r[1][index] * B_r[index][3]);
+
+            C_r[2][0] <= C_r[2][0] + (A_r[2][index] * B_r[index][0]);
+            C_r[2][1] <= C_r[2][1] + (A_r[2][index] * B_r[index][1]);
+            C_r[2][2] <= C_r[2][2] + (A_r[2][index] * B_r[index][2]);
+            C_r[2][3] <= C_r[2][3] + (A_r[2][index] * B_r[index][3]);
+
+            C_r[3][0] <= C_r[3][0] + (A_r[3][index] * B_r[index][0]);
+            C_r[3][1] <= C_r[3][1] + (A_r[3][index] * B_r[index][1]);
+            C_r[3][2] <= C_r[3][2] + (A_r[3][index] * B_r[index][2]);
+            C_r[3][3] <= C_r[3][3] + (A_r[3][index] * B_r[index][3]);
+
+            index <= index + 1'b1;
+        end
+    end
+
+    // Register outputs
+    always_ff @(posedge clk) begin
+        if (~rstn) begin
+            o_dv_r <= 1'b0;
+            o_ready_r <= 1'b1;
+        end
+        else begin
+            if (current_state == IDLE) begin
+                o_ready_r <= 1'b1;
+                o_dv_r <= 1'b0;
+                foreach (C[i, j]) C[i][j] <= '0;
+            end else if (current_state == PROCESSING) begin
+                foreach (C[i, j]) C[i][j] <= '0;
+                o_ready_r <= 1'b0;
+                o_dv_r <= 1'b0;
+            end
+            else begin
+                C[0][0] <= {C_r[0][0][DATAWIDTH-1], C_r[0][0][OutputRangeEnd-1:OutputRangeStart]};
+                C[0][1] <= {C_r[0][0][DATAWIDTH-1], C_r[0][1][OutputRangeEnd-1:OutputRangeStart]};
+                C[0][2] <= {C_r[0][0][DATAWIDTH-1], C_r[0][2][OutputRangeEnd-1:OutputRangeStart]};
+                C[0][3] <= {C_r[0][0][DATAWIDTH-1], C_r[0][3][OutputRangeEnd-1:OutputRangeStart]};
+
+                C[1][0] <= {C_r[0][0][DATAWIDTH-1], C_r[1][0][OutputRangeEnd-1:OutputRangeStart]};
+                C[1][1] <= {C_r[0][0][DATAWIDTH-1], C_r[1][1][OutputRangeEnd-1:OutputRangeStart]};
+                C[1][2] <= {C_r[0][0][DATAWIDTH-1], C_r[1][2][OutputRangeEnd-1:OutputRangeStart]};
+                C[1][3] <= {C_r[0][0][DATAWIDTH-1], C_r[1][3][OutputRangeEnd-1:OutputRangeStart]};
+
+                C[2][0] <= {C_r[0][0][DATAWIDTH-1], C_r[2][0][OutputRangeEnd-1:OutputRangeStart]};
+                C[2][1] <= {C_r[0][0][DATAWIDTH-1], C_r[2][1][OutputRangeEnd-1:OutputRangeStart]};
+                C[2][2] <= {C_r[0][0][DATAWIDTH-1], C_r[2][2][OutputRangeEnd-1:OutputRangeStart]};
+                C[2][3] <= {C_r[0][0][DATAWIDTH-1], C_r[2][3][OutputRangeEnd-1:OutputRangeStart]};
+
+                C[3][0] <= {C_r[0][0][DATAWIDTH-1], C_r[3][0][OutputRangeEnd-1:OutputRangeStart]};
+                C[3][1] <= {C_r[0][0][DATAWIDTH-1], C_r[3][1][OutputRangeEnd-1:OutputRangeStart]};
+                C[3][2] <= {C_r[0][0][DATAWIDTH-1], C_r[3][2][OutputRangeEnd-1:OutputRangeStart]};
+                C[3][3] <= {C_r[0][0][DATAWIDTH-1], C_r[3][3][OutputRangeEnd-1:OutputRangeStart]};
+
+                o_dv_r <= 1'b1;
+                o_ready_r <= 1'b1;
+            end
+        end
+    end
+
+    assign o_ready = o_ready_r;
+    assign o_dv = o_dv_r;
+endmodule
---
+++ lib/Math/MatMul/tb/tb_mat_mul.cpp
@@ -0,0 +1,169 @@
+#include <cstdlib>
+#include <verilated.h>
+#include <verilated_vcd_c.h>
+#include "../../verilator_lib/fixed_point.h"
+
+#include "obj_dir/Vmat_mul.h"
+#include "obj_dir/Vmat_mul___024unit.h"
+
+#define FIXED_POINT_WIDTH 24
+#define FIXED_POINT_FRAC_WIDTH 13
+
+#define RESET_CLKS 8
+
+float test_A[4][4] = {
+    {1.0, 2.0, 3.0, 4.0},
+    {1.0, 2.0, 3.0, 4.0},
+    {1.0, 2.0, 3.0, 4.0},
+    {1.0, 2.0, 3.0, 4.0}
+};
+
+float test_B[4][4] = {
+    {2.0, 0.0, 0.0, 0.0},
+    {0.0, 0.5, 0.0, 0.0},
+    {0.0, 0.0, 0.25, 0.0},
+    {0.0, 0.0, 0.0, 4.76623}
+};
+
+// float test_A[4][4] = {
+//     {1.0, 2.0, 3.0, 4.0},
+//     {2.2, 3.2, 2.6, 1.8},
+//     {2.9, 4.8, 5.6, 2.4},
+//     {0.1, 0.8, 8.2, 6.3}
+// };
+//
+// float test_B[4][4] = {
+//     {8.8, 9.2, 5.2, 1.5},
+//     {9.3, 6.3, 7.3, 6.1}, 
+//     {4.4, 3.2, 2.2, 5.6},
+//     {7.9, 1.2, 4.5, 6.2}
+// };
+
+void print_matrix(float mat[4][4]);
+
+#define MAX_SIM_TIME 40
+vluint64_t sim_time = 0;
+vluint64_t posedge_cnt = 0;
+
+void populate_matrix_random(float mat[4][4], float max_val) {
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+        }
+    }
+}
+
+void assign_matrix_data(Vmat_mul* dut, float A[4][4], float B[4][4]) {
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            dut->A[i][j] = FixedPoint<vluint32_t>::fromFloat(A[i][j], FIXED_POINT_FRAC_WIDTH, FIXED_POINT_WIDTH).get();
+            dut->B[i][j] = FixedPoint<vluint32_t>::fromFloat(B[i][j], FIXED_POINT_FRAC_WIDTH, FIXED_POINT_WIDTH).get();
+        }
+    }
+};
+
+void get_matrix_data_input(Vmat_mul* dut, float A[4][4], float B[4][4]) {
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            A[i][j] = FixedPoint<vluint32_t>(dut->A[i][j], FIXED_POINT_FRAC_WIDTH, FIXED_POINT_WIDTH).toFloat();
+            B[i][j] = FixedPoint<vluint32_t>(dut->B[i][j], FIXED_POINT_FRAC_WIDTH, FIXED_POINT_WIDTH).toFloat();
+        }
+    }
+};
+
+void get_matrix_data_output(Vmat_mul* dut, float C[4][4]) {
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            C[i][j] = FixedPoint<vluint32_t>(dut->C[i][j], FIXED_POINT_FRAC_WIDTH, FIXED_POINT_WIDTH).toFloat();
+        }
+    }
+}
+
+void check_matrix_data(Vmat_mul* dut) {
+    static float A[4][4];
+    static float B[4][4];
+
+    if (dut->o_dv == 1) {
+        float C[4][4];
+        get_matrix_data_output(dut, C);
+
+        printf("\n================\nClock: %ld \n================\n", posedge_cnt);
+        printf("Sendt in matrix A:\n");
+        print_matrix(A);
+        printf("Sendt in matrix B:\n");
+        print_matrix(B);
+        printf("Received matrix C:\n");
+        print_matrix(C);
+    }
+
+    if (dut->i_dv == 1) {
+        get_matrix_data_input(dut, A, B);
+    }
+
+};
+
+int main(int argc, char** argv) {
+    Verilated::commandArgs(argc, argv);
+    Vmat_mul* dut = new Vmat_mul;
+
+    Verilated::traceEverOn(true);
+    VerilatedVcdC* m_trace = new VerilatedVcdC;
+    dut->trace(m_trace, 5);
+    m_trace->open("waveform.vcd");
+
+    printf("=======================\n");
+    printf("# Starting simulation #\n");
+    printf("=======================\n");
+
+    // Reset DUT
+    for (int i = 0; i < RESET_CLKS; i++) {
+        dut->clk ^= 1;
+        dut->eval();
+
+        dut->rstn = 0;
+        dut->i_dv = 0;
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                dut->A[i][j] = 0;
+                dut->B[i][j] = 0;
+            }
+        }
+
+        m_trace->dump(sim_time);
+        sim_time++;
+    }
+    dut->rstn = 1;
+
+    // Start sim
+    while (sim_time < MAX_SIM_TIME) {
+        dut->clk ^= 1;
+        dut->eval();
+
+        if (dut->clk == 1) {
+            posedge_cnt++;
+            dut->i_dv = 0;
+
+            if (posedge_cnt == 2) {
+                assign_matrix_data(dut, test_A, test_B);
+                dut->i_dv = 1;
+            }
+
+            check_matrix_data(dut);
+        }
+
+        m_trace->dump(sim_time);
+        sim_time++;
+    }
+
+    m_trace->close();
+    delete dut;
+    exit(EXIT_SUCCESS);
+}
+
+void print_matrix(float mat[4][4]) {
+    printf("%f, %f, %f, %f\n%f, %f, %f, %f\n%f, %f, %f, %f\n%f, %f, %f, %f\n", 
+        mat[0][0], mat[0][1], mat[0][2], mat[0][3],
+        mat[1][0], mat[1][1], mat[1][2], mat[1][3],
+        mat[2][0], mat[2][1], mat[2][2], mat[2][3],
+        mat[3][0], mat[3][1], mat[3][2], mat[3][3]
+    );
+}
