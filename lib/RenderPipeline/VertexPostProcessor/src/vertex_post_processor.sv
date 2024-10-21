5dd0dbce6ef8ab2238452b239e7c87b04fc67cd8
Morten Sørensen <morten.sorensen.04@gmail.com>
2024-10-21 13:50:35 +0200

    reworked vertex post-processor to include basic clipping

--- lib/RenderPipeline/VertexPostProcessor/src/vertex_post_processor.sv
+++ lib/RenderPipeline/VertexPostProcessor/src/vertex_post_processor.sv
@@ -9,45 +9,74 @@ typedef enum logic [2:0] {
     VPP_IDLE,
     VPP_CLIP,
     VPP_PERSPECTIVE_DIVIDE,
-    VPP_DONE,
+    VPP_SCREEN_SPACE_TRANSFORM,
     VPP_ERROR_STATE
 } vertex_post_processor_state_t;
 
 module vertex_post_processor #(
-        parameter unsigned DATAWIDTH = 24,
-        parameter unsigned FRACBITS = 13
+        parameter unsigned IV_DATAWIDTH = 24,
+        parameter unsigned IV_FRACBITS = 13,
+
+        parameter unsigned OV_DATAWIDTH = 12,
+        parameter unsigned O_DEPTH_FRACBITS = 12,  // Q0.12
+
+        logic signed [IV_DATAWIDTH-1:0] WIDTH = 320,
+        logic signed [IV_DATAWIDTH-1:0] HEIGHT = 320,
+
+        parameter real ZFAR = 100.0,
+        parameter real ZNEAR = 0.1
     ) (
         input logic clk,
         input logic rstn,
         output logic ready,
 
-        input logic signed [DATAWIDTH-1:0] i_vertex[4],
+        input logic signed [IV_DATAWIDTH-1:0] i_vertex[4],
         input logic i_vertex_dv,
 
-        output logic signed [DATAWIDTH-1:0] o_vertex[3],
+        output logic signed [OV_DATAWIDTH-1:0] o_vertex_pixel[2],
+        output logic unsigned [O_DEPTH_FRACBITS-1:0] o_vertex_z,
         output logic done,
         output logic invalid
     );
+
+    localparam logic signed [IV_DATAWIDTH-1:0] FP_One = (1 << IV_FRACBITS);
+    localparam logic signed [IV_DATAWIDTH-1:0] WIDTH_FP = (WIDTH << IV_FRACBITS);
+    localparam logic signed [IV_DATAWIDTH-1:0] HEIGHT_FP = (HEIGHT << IV_FRACBITS);
+    localparam logic signed [2 * IV_DATAWIDTH:0] WIDTH_FP_D = WIDTH_FP * FP_One;
+    localparam logic signed [2 * IV_DATAWIDTH:0] HEIGHT_FP_D = HEIGHT_FP * FP_One;
+
+    localparam unsigned OutPixIndStart = 2 * IV_FRACBITS;
+    localparam unsigned OutPixIndEnd = OV_DATAWIDTH + 2 * IV_FRACBITS - 2;
 
     // State
     vertex_post_processor_state_t current_state = VPP_IDLE, next_state = VPP_IDLE;
 
     // Register input vertex data
-    logic signed [DATAWIDTH-1:0] r_clip_x;
-    logic signed [DATAWIDTH-1:0] r_clip_y;
-    logic signed [DATAWIDTH-1:0] r_clip_z;
-    logic signed [DATAWIDTH-1:0] r_clip_w;
+    logic signed [IV_DATAWIDTH-1:0] r_clip_x;
+    logic signed [IV_DATAWIDTH-1:0] r_clip_y;
+    logic signed [IV_DATAWIDTH-1:0] r_clip_z;
+    logic signed [IV_DATAWIDTH-1:0] r_clip_w;
+
+    // NDC Data
+    /* verilator lint_off UNUSED */         // Some bits are not used
+    logic signed [IV_DATAWIDTH-1:0] w_ndc_x;
+    logic signed [IV_DATAWIDTH-1:0] w_ndc_y;
+    logic signed [IV_DATAWIDTH-1:0] w_ndc_z;
+    /* verilator lint_on UNUSED */          // Some bits are not used
+
+    // Screen-Space Data
+    logic signed [2 * IV_DATAWIDTH:0] w_ss_x_inter;
+    logic signed [2 * IV_DATAWIDTH:0] w_ss_y_inter;
 
     // CLIP-NDC Signals
-    logic signed [DATAWIDTH-1:0] w_ndc_x;
-    logic signed [DATAWIDTH-1:0] w_ndc_y;
-    logic signed [DATAWIDTH-1:0] w_ndc_z;
-
     logic w_ndc_x_valid, w_ndc_y_valid, w_ndc_z_valid;
     logic w_ndc_x_busy, w_ndc_z_busy, w_ndc_y_busy;
     logic w_ndc_x_done, w_ndc_y_done, w_ndc_z_done;
+
+    /* verilator lint_off UNUSED */         // May be used later
     logic w_ndc_x_dbz, w_ndc_y_dbz, w_ndc_z_dbz;
     logic w_ndc_x_ovf, w_ndc_y_ovf, w_ndc_z_ovf;
+    /* verilator lint_off UNUSED */         // May be used later
 
     // Start signal for perspective divide
     logic ndc_divide_start;
@@ -61,13 +90,19 @@ module vertex_post_processor #(
     // Check z clipping
     logic z_invalid;
     always_comb begin
-        z_invalid = (0 < r_clip_z) || (r_clip_z < r_clip_w);
+        z_invalid = ((r_clip_z <= 0) || (r_clip_w <= r_clip_z)) & (current_state == VPP_CLIP);
+    end
+
+    // NDC-Screen Transform
+    always_comb begin
+        w_ss_x_inter = w_ndc_x * WIDTH_FP + WIDTH_FP_D;
+        w_ss_y_inter = HEIGHT_FP_D - w_ndc_y * HEIGHT_FP;
     end
 
     // Clip to ndc dividers
     fixed_point_divide #(
-        .WIDTH(DATAWIDTH),
-        .FRACBITS(FRACBITS)
+        .WIDTH(IV_DATAWIDTH),
+        .FRACBITS(IV_FRACBITS)
     ) ndc_divide_x_inst (
         .clk(clk),
         .rstn(rstn),
@@ -87,8 +122,8 @@ module vertex_post_processor #(
     );
 
     fixed_point_divide #(
-        .WIDTH(DATAWIDTH),
-        .FRACBITS(FRACBITS)
+        .WIDTH(IV_DATAWIDTH),
+        .FRACBITS(IV_FRACBITS)
     ) ndc_divide_y_inst (
         .clk(clk),
         .rstn(rstn),
@@ -108,8 +143,8 @@ module vertex_post_processor #(
     );
 
     fixed_point_divide #(
-        .WIDTH(DATAWIDTH),
-        .FRACBITS(FRACBITS)
+        .WIDTH(IV_DATAWIDTH),
+        .FRACBITS(IV_FRACBITS)
     ) ndc_divide_z_inst (
         .clk(clk),
         .rstn(rstn),
@@ -149,22 +184,27 @@ module vertex_post_processor #(
     // Set output data and signals
     always_ff @(posedge clk) begin
         if (~rstn) begin
-            foreach (o_vertex[i]) o_vertex[i] <= '0;
+            foreach (o_vertex_pixel[i]) o_vertex_pixel[i] <= '0;
+            o_vertex_z <= '0;
+            done <= '0;
+            invalid <= '0;
         end else begin
             case (current_state)
-                VPP_DONE: begin
-                    o_vertex[0] <= w_ndc_x;
-                    o_vertex[1] <= w_ndc_y;
-                    o_vertex[2] <= w_ndc_z;
+                VPP_SCREEN_SPACE_TRANSFORM: begin
+                    o_vertex_pixel[0] <= {w_ss_x_inter[2 * IV_DATAWIDTH - 1], w_ss_x_inter[OutPixIndEnd + 1:OutPixIndStart + 1]};
+                    o_vertex_pixel[1] <= {w_ss_y_inter[2 * IV_DATAWIDTH - 1], w_ss_y_inter[OutPixIndEnd+1:OutPixIndStart+1]};
+                    o_vertex_z <= w_ndc_z[IV_FRACBITS-1:IV_FRACBITS-O_DEPTH_FRACBITS];
                     done <= '1;
                 end
 
                 VPP_ERROR_STATE: begin
                     done <= '1;
+                    invalid <= '1;
                 end
 
                 default: begin
                     done <= '0;
+                    invalid <= '0;
                 end
             endcase
         end
@@ -181,14 +221,14 @@ module vertex_post_processor #(
 
     always_comb begin
         next_state = current_state;
-        invalid = 0;
-        ready = 1;
+        ready = 0;
 
         case (current_state)
             VPP_IDLE: begin
                 if (i_vertex_dv) begin
                     next_state = VPP_CLIP;
-                    ready = 0;
+                end else begin
+                    ready = 1;
                 end
             end
 
@@ -198,26 +238,24 @@ module vertex_post_processor #(
                 end else begin
                     next_state = VPP_PERSPECTIVE_DIVIDE;
                 end
-                ready = 0;
             end
 
             VPP_PERSPECTIVE_DIVIDE: begin
-                if (w_ndc_x_valid & w_ndc_y_valid & w_ndc_z_valid) begin
-                    next_state = VPP_ERROR_STATE;
-                end else if (w_ndc_x_done && w_ndc_y_done && w_ndc_z_done) begin
-                    next_state = VPP_DONE;
+                if (w_ndc_x_done && w_ndc_y_done && w_ndc_z_done) begin
+                    if (!w_ndc_x_valid || !w_ndc_y_valid || !w_ndc_z_valid) begin
+                        next_state = VPP_ERROR_STATE;
+                    end else begin
+                        next_state = VPP_SCREEN_SPACE_TRANSFORM;
+                    end
                 end
-
-                ready = 0;
             end
 
-            VPP_DONE: begin
+            VPP_SCREEN_SPACE_TRANSFORM: begin
                 next_state = VPP_IDLE;
             end
 
             VPP_ERROR_STATE: begin
-                invalid = 1;
-                ready = 0;
+                next_state = VPP_IDLE;
             end
 
             default:
--- lib/RenderPipeline/VertexPostProcessor/tb/tb_vertex_post_processor.cpp
+++ lib/RenderPipeline/VertexPostProcessor/tb/tb_vertex_post_processor.cpp
@@ -5,11 +5,11 @@
 
 #include "obj_dir/Vvertex_post_processor.h"
 
-#define FIXED_POINT_WIDTH 24
-#define FIXED_POINT_FRAC_WIDTH 13
+#define IV_DATAWIDTH 24
+#define IV_FRACBITS 13
 
-#define Z_BITS 12
-#define Z_FRAC_BITS 11
+#define OV_DATAWIDTH 12
+#define O_DEPTH_FRACBITS 12 // Q0.12
 
 #define RESET_CLKS 8
 
@@ -21,12 +21,23 @@ vluint64_t sim_time = 0;
 vluint64_t posedge_cnt = 0;
 
 void assign_input_vertex(Vvertex_post_processor* dut, float vertex[4]) {
-    dut->i_vertex[0] = FixedPoint<int32_t>::fromFloat(vertex[0], FIXED_POINT_FRAC_WIDTH, FIXED_POINT_WIDTH).get();
-    dut->i_vertex[1] = FixedPoint<int32_t>::fromFloat(vertex[1], FIXED_POINT_FRAC_WIDTH, FIXED_POINT_WIDTH).get();
-    dut->i_vertex[2] = FixedPoint<int32_t>::fromFloat(vertex[2], FIXED_POINT_FRAC_WIDTH, FIXED_POINT_WIDTH).get();
-    dut->i_vertex[3] = FixedPoint<int32_t>::fromFloat(vertex[3], FIXED_POINT_FRAC_WIDTH, FIXED_POINT_WIDTH).get();
+    dut->i_vertex[0] = FixedPoint<int32_t>::fromFloat(vertex[0], IV_FRACBITS, IV_DATAWIDTH).get();
+    dut->i_vertex[1] = FixedPoint<int32_t>::fromFloat(vertex[1], IV_FRACBITS, IV_DATAWIDTH).get();
+    dut->i_vertex[2] = FixedPoint<int32_t>::fromFloat(vertex[2], IV_FRACBITS, IV_DATAWIDTH).get();
+    dut->i_vertex[3] = FixedPoint<int32_t>::fromFloat(vertex[3], IV_FRACBITS, IV_DATAWIDTH).get();
 
     dut->i_vertex_dv = 1;
+}
+
+int32_t sign_extend(int32_t a, int data_width) {
+    int32_t sign = (a >> (data_width - 1)) & 1;
+    int32_t sign_extended = a;
+    if (sign) {
+        for (int i = sizeof(int32_t) * 8 - 1; i >= data_width; i--) {
+            sign_extended |= (1 << i);
+        }
+    }
+    return sign_extended; 
 }
 
 int main(int argc, char** argv) {
@@ -59,7 +70,7 @@ int main(int argc, char** argv) {
     int screen_height = 320;
 
     float test_vector[4] = {
-        -2.3, -1.635, 0.8875, 3.23302
+        -2.3, -1.635, 0.8875, 0.9
     };
 
     float expected_ndc[3] = {0.0f};
@@ -73,14 +84,9 @@ int main(int argc, char** argv) {
     expected_screen_pixels[1] = int((1 - expected_ndc[1]) * screen_height / 2);
     expected_depth = expected_ndc[2];
 
-    printf("Expected: \n");
-    printf("\tNDC: ");
-    print_vector3(expected_ndc);
-    printf("\tResult:\n");
-    printf("\t\tDepth: %f\n", expected_depth);
-    printf("\t\tScreen Pixels: (%d, %d)\n", expected_screen_pixels[0], expected_screen_pixels[1]);
-    
-    printf("========== STARTING SIMULATION ==========\n");
+    printf("Expected:");
+    printf("\t(%d, %d), %f\n", expected_screen_pixels[0], expected_screen_pixels[1], expected_depth);
+
     while (sim_time < MAX_SIM_TIME) {
         dut->clk ^= 1;
         dut->eval();
@@ -90,19 +96,25 @@ int main(int argc, char** argv) {
             dut->i_vertex_dv = 0;
 
             if (posedge_cnt == 4) {
-                assign_input_vertex(dut, test_vector);
+                if (dut->ready) {
+                    assign_input_vertex(dut, test_vector);
+                }
             }
 
-            if (dut->o_vertex_dv) {
-                int32_t vertex_pix[2] = {0};
-                vertex_pix[0] = dut->o_vertex_pixel[0];
-                vertex_pix[1] = dut->o_vertex_pixel[1];
+            if (dut->done) {
+                int32_t vertex_pixel[2];
+                float vertex_depth;
 
-                float vertex_z = 0.0f;
-                vertex_z = FixedPoint<int32_t>(dut->o_vertex_z, Z_FRAC_BITS, Z_BITS).toFloat();
+                if (dut->invalid) {
+                    printf("Got invalid vertex\n");
+                } else {
+                    vertex_pixel[0] = sign_extend(dut->o_vertex_pixel[0], OV_DATAWIDTH);
+                    vertex_pixel[1] = sign_extend(dut->o_vertex_pixel[1], OV_DATAWIDTH);
+                    vertex_depth = FixedPoint<int32_t>(dut->o_vertex_z, O_DEPTH_FRACBITS, O_DEPTH_FRACBITS+1).toFloat();
 
-                printf("\n\nGot: \n");
-                printf("\tPixel: (%d, %d)\t Depth: %f\n", vertex_pix[0], vertex_pix[1], vertex_z);
+                    printf("Got:\t\t(%d, %d), %f\n", vertex_pixel[0], vertex_pixel[1], vertex_depth);
+                }
+
             }
         }
     
