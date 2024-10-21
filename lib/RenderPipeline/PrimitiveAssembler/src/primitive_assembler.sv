7d921ed4b0596d7c665d2dfec71640386ab33418
Morten Sørensen <morten.sorensen.04@gmail.com>
2024-10-21 20:09:56 +0200

    working primitive assembler

--- lib/RenderPipeline/PrimitiveAssembler/src/primitive_assembler.sv
+++ lib/RenderPipeline/PrimitiveAssembler/src/primitive_assembler.sv
@@ -1,11 +1,18 @@
 `timescale 1ns / 1ps
 
-module primitive_assembler #(
-        parameter unsigned INPUT_VERTEX_DATAWIDTH = 24,
-        parameter unsigned INPUT_VERTEX_FRACBITS = 13,
+typedef enum logic [2:0] {
+    PA_IDLE,
+    PA_ASSEMBLE,
+    PA_WAIT_LAST,
+    PA_DONE
+} pa_state_t;
 
-        parameter unsigned OUTPUT_VERTEX_DATAWIDTH = 12,
-        parameter unsigned OUTPUT_VERTEX_DEPTH_FRACBITS = 12,   // Unsigned Q0.12 format
+module primitive_assembler #(
+        parameter unsigned IV_DATAWIDTH = 12,
+        parameter unsigned IV_DEPTH_FRACBITS = 12,   // Unsigned Q0.12 format
+
+        parameter logic signed [IV_DATAWIDTH-1:0] SCREEN_WIDTH = 320,
+        parameter logic signed [IV_DATAWIDTH-1:0] SCREEN_HEIGHT = 320,
 
         parameter unsigned MAX_TRIANGLE_COUNT = 2048
     ) (
@@ -20,28 +27,237 @@ module primitive_assembler #(
         input logic [$clog2(MAX_TRIANGLE_COUNT)-1:0] i_num_triangles,
 
         // Index Buffer
-        output logic [$clog2(MAX_TRIANGLE_COUNT)-1:0] o_triangle_addr,
-        output logic o_triangle_read_en,
+        output logic [$clog2(MAX_TRIANGLE_COUNT)-1:0] o_index_buff_addr,
+        output logic o_index_buff_read_en,
 
-        input logic [$clog2(3 * MAX_TRIANGLE_COUNT)-1:0] i_triangle_idx[3],
+        input logic [$clog2(3 * MAX_TRIANGLE_COUNT)-1:0] i_vertex_idxs[3],
 
         // Vertex Transform Buffer
-        output logic [$clog2(3 * MAX_TRIANGLE_COUNT)-1:0] o_vertex_addr,
+        output logic [$clog2(3 * MAX_TRIANGLE_COUNT)-1:0] o_vertex_addr[3], // Do 3 reads at a time
         output logic o_vertex_read_en,
 
-        input logic signed [INPUT_VERTEX_DATAWIDTH - 1:0] i_vertex[3],
-        input logic i_vertex_invalid,                                       // Vertex invalid flag
-                                                                            // if 1, don't render primitive
+        input logic signed [IV_DATAWIDTH - 1:0] i_v0[2],
+        input logic signed [IV_DEPTH_FRACBITS-1:0] i_v0_z,
+        input logic i_v0_invalid,                                   // Vertex invalid flag
+                                                                    // if 1, don't render primitive
+        input logic signed [IV_DATAWIDTH - 1:0] i_v1[2],
+        input logic signed [IV_DEPTH_FRACBITS-1:0] i_v1_z,
+        input logic i_v1_invalid,                                   // Vertex invalid flag
+                                                                    // if 1, don't render primitive
+        input logic signed [IV_DATAWIDTH - 1:0] i_v2[2],
+        input logic signed [IV_DEPTH_FRACBITS-1:0] i_v2_z,
+        input logic i_v2_invalid,                                   // Vertex invalid flag
+                                                                    // if 1, don't render primitive
 
         // Output primitive
-        output logic signed [OUTPUT_VERTEX_DATAWIDTH-1:0] o_vertex_pixel[3][2],
-        output logic unsigned [OUTPUT_VERTEX_DEPTH_FRACBITS-1:0] o_vertex_z[3],
+        output logic signed [IV_DATAWIDTH-1:0] o_vertex_pixel[3][2],
+        output logic unsigned [IV_DEPTH_FRACBITS-1:0] o_vertex_z[3],
 
         // Primitive bounding box
-        output logic signed [OUTPUT_VERTEX_DATAWIDTH-1:0] bb_tl[2],
-        output logic signed [OUTPUT_VERTEX_DATAWIDTH-1:0] bb_br[2],
+        output logic signed [IV_DATAWIDTH-1:0] bb_tl[2],
+        output logic signed [IV_DATAWIDTH-1:0] bb_br[2],
 
         output logic o_dv
     );
+
+    logic [$clog2(MAX_TRIANGLE_COUNT)-1:0] r_num_triangles = '0;
+    logic [$clog2(MAX_TRIANGLE_COUNT)-1:0] r_triangle_cnt = '0;
+
+    // Check if triangle index buffer was read last clk
+    logic r_index_buff_read_last = '0;
+    logic r_vertex_read_last = '0;
+    logic r_finished_wait_last = '0;
+
+    // Calculate primitive bounding box
+    logic [IV_DATAWIDTH-1:0] w_bb_tl[2];
+    logic [IV_DATAWIDTH-1:0] w_bb_br[2];
+    logic w_bb_valid;
+
+    bounding_box #(
+        .TILE_MIN_X(0),
+        .TILE_MAX_X(SCREEN_WIDTH),
+        .TILE_MIN_Y(0),
+        .TILE_MAX_Y(SCREEN_HEIGHT),
+        .COORD_WIDTH(IV_DATAWIDTH)
+    ) bb_inst (
+        .x0(i_v0[0]),
+        .y0(i_v0[1]),
+        .x1(i_v1[0]),
+        .y1(i_v1[1]),
+        .x2(i_v2[0]),
+        .y2(i_v2[1]),
+
+        .min_x(w_bb_tl[0]),
+        .max_x(w_bb_br[0]),
+        .min_y(w_bb_tl[1]),
+        .max_y(w_bb_br[1]),
+
+        .valid(w_bb_valid)
+    );
+
+    // State
+    pa_state_t current_state = PA_IDLE, next_state = PA_IDLE;
+    always_ff @(posedge clk) begin
+        if (~rstn) begin
+            current_state <= PA_IDLE;
+        end else begin
+            current_state <= next_state;
+        end
+    end
+
+    always_comb begin
+        next_state = current_state;
+
+        finished = 0;
+        ready = 0;
+
+        case (current_state)
+            PA_IDLE: begin
+                if (start) begin
+                    next_state = PA_ASSEMBLE;
+                end else begin
+                    ready = 1;
+                end
+            end
+
+            PA_ASSEMBLE: begin
+                if (r_triangle_cnt == r_num_triangles) begin
+                    next_state = PA_WAIT_LAST;
+                end
+            end
+
+            PA_WAIT_LAST: begin
+                if (r_finished_wait_last) begin
+                    next_state = PA_DONE;
+                end
+            end
+
+            PA_DONE: begin
+                finished = 1;
+                next_state = PA_IDLE;
+            end
+
+            default: begin
+                next_state = PA_IDLE;
+            end
+        endcase
+    end
+
+    always_ff @(posedge clk) begin
+        if (~rstn) begin
+            r_num_triangles <= '0;
+            r_triangle_cnt <= '0;
+
+            o_index_buff_read_en <= '0;
+            r_index_buff_read_last <= '0;
+
+            foreach (o_vertex_addr[i]) o_vertex_addr[i] <= '0;
+            o_vertex_read_en <= '0;
+
+            r_vertex_read_last <= '0;
+            r_finished_wait_last <= '0;
+
+            foreach (o_vertex_pixel[i,j]) o_vertex_pixel[i][j] <= '0;
+            foreach (o_vertex_pixel[i,j]) o_vertex_pixel[i][j] <= '0;
+            foreach (o_vertex_z[i]) o_vertex_z[i] <= '0;
+            o_dv <= '0;
+        end else begin
+            case (current_state)
+                PA_IDLE: begin
+                    if (start) begin
+                        r_num_triangles <= i_num_triangles;
+                    end
+                    r_triangle_cnt <= '0;
+                    r_vertex_read_last <= '0;
+                end
+
+                PA_ASSEMBLE: begin
+                    r_triangle_cnt <= r_triangle_cnt + 1;
+                    o_index_buff_addr <= r_triangle_cnt;
+                    o_index_buff_read_en <= '1;
+                    r_index_buff_read_last <= '1;
+
+                    if (r_index_buff_read_last) begin
+                        foreach (o_vertex_addr[i]) o_vertex_addr[i] <= i_vertex_idxs[i];
+                        o_vertex_read_en <= '1;
+                        r_vertex_read_last <= '1;
+                    end else begin
+                        r_vertex_read_last <= '0;
+                        o_vertex_read_en <= '0;
+                    end
+
+                    if (r_vertex_read_last) begin
+                        o_vertex_pixel[0][0] <= i_v0[0];
+                        o_vertex_pixel[0][1] <= i_v0[1];
+                        o_vertex_z[0] <= i_v0_z;
+
+                        o_vertex_pixel[1][0] <= i_v1[0];
+                        o_vertex_pixel[1][1] <= i_v1[1];
+                        o_vertex_z[1] <= i_v1_z;
+
+                        o_vertex_pixel[2][0] <= i_v2[0];
+                        o_vertex_pixel[2][1] <= i_v2[1];
+                        o_vertex_z[2] <= i_v2_z;
+
+                        bb_tl[0] <= w_bb_tl[0];
+                        bb_tl[1] <= w_bb_tl[1];
+
+                        bb_br[0] <= w_bb_br[0];
+                        bb_br[1] <= w_bb_br[1];
+
+                        if (w_bb_valid & !i_v0_invalid & !i_v1_invalid & !i_v2_invalid) begin
+                            o_dv <= '1;
+                        end else begin
+                            o_dv <= '0;
+                        end
+                    end
+                end
+
+                PA_WAIT_LAST: begin
+                    if (r_index_buff_read_last) begin
+                        foreach (o_vertex_addr[i]) o_vertex_addr[i] <= i_vertex_idxs[i];
+                        o_vertex_read_en <= '0;
+                        r_vertex_read_last <= '1;
+                    end
+
+                    if (r_vertex_read_last) begin
+                        o_vertex_pixel[0][0] <= i_v0[0];
+                        o_vertex_pixel[0][1] <= i_v0[1];
+                        o_vertex_z[0] <= i_v0_z;
+
+                        o_vertex_pixel[1][0] <= i_v1[0];
+                        o_vertex_pixel[1][1] <= i_v1[1];
+                        o_vertex_z[1] <= i_v1_z;
+
+                        o_vertex_pixel[2][0] <= i_v2[0];
+                        o_vertex_pixel[2][1] <= i_v2[1];
+                        o_vertex_z[2] <= i_v2_z;
+
+                        bb_tl[0] <= w_bb_tl[0];
+                        bb_tl[1] <= w_bb_tl[1];
+
+                        bb_br[0] <= w_bb_br[0];
+                        bb_br[1] <= w_bb_br[1];
+
+                        if (w_bb_valid & !i_v0_invalid & !i_v1_invalid & !i_v2_invalid) begin
+                            o_dv <= '1;
+                        end else begin
+                            o_dv <= '0;
+                        end
+
+                        r_finished_wait_last <= '1;
+                    end else begin
+                        r_finished_wait_last <= '0;
+                    end
+                end
+
+                default: begin
+                    o_index_buff_read_en <= '0;
+                    r_index_buff_read_last <= '0;
+                    r_vertex_read_last <= '0;
+                end
+            endcase
+        end
+    end
 
 endmodule
---
+++ lib/RenderPipeline/PrimitiveAssembler/tb/tb_primitive_assembler.cpp
@@ -0,0 +1,211 @@
+#include <cstdlib>
+#include <verilated.h>
+#include <verilated_vcd_c.h>
+#include "../../../../verilator_utils/fixed_point.h"
+
+#include "obj_dir/Vprimitive_assembler.h"
+
+#define IV_DATAWIDTH 24
+#define IV_FRACBITS 13
+
+#define OV_DATAWIDTH 12
+#define O_DEPTH_FRACBITS 12 // Q0.12
+
+#define RESET_CLKS 8
+
+#define MAX_SIM_TIME 240
+vluint64_t sim_time = 0;
+vluint64_t posedge_cnt = 0;
+
+typedef struct {
+    int32_t x;
+    int32_t y;
+    float z;
+    int invalid;
+} Vertex;
+
+typedef struct {
+    int32_t i0;
+    int32_t i1;
+    int32_t i2;
+} Triangle;
+
+int32_t vertex_pixel_data_size = 8;
+Vertex vertex_pixel_data[] = {
+    {148, 160, 0.981378f, 0},
+    {191, 222, 0.979009f, 0},
+    {122, 234, 0.974551f, 0},
+    {80,  160, 0.977861f, 0},
+    {191,  97, 0.979009f, 0},
+    {245, 160, 0.976026f, 0},
+    {177, 160, 0.970188f, 0},
+    {122,  85, 0.974551f, 0},
+};
+
+int32_t index_data_size = 12;
+Triangle index_data[] = {
+    {1, 3, 0}, 
+    {7, 5, 4}, 
+    {4, 1, 0}, 
+    {5, 2, 1}, 
+    {2, 7, 3}, 
+    {0, 7, 4}, 
+    {1, 2, 3}, 
+    {7, 6, 5}, 
+    {4, 5, 1}, 
+    {5, 6, 2}, 
+    {2, 6, 7}, 
+    {0, 3, 7}
+};
+
+int32_t sign_extend(int32_t a, int data_width) {
+    int32_t sign = (a >> (data_width - 1)) & 1;
+    int32_t sign_extended = a;
+    if (sign) {
+        for (int i = sizeof(int32_t) * 8 - 1; i >= data_width; i--) {
+            sign_extended |= (1 << i);
+        }
+    }
+    return sign_extended; 
+}
+
+// Turn int32_t into lower datawidth
+int32_t truncate(int32_t a, int data_width) {
+    int32_t truncated = a & ((1 << data_width) - 1);
+    return truncated;
+}
+
+void assign_input_index(Vprimitive_assembler* dut) {
+    if (dut->o_index_buff_read_en) {
+        if (dut->o_index_buff_addr >= index_data_size) {
+            return;
+        }
+
+        dut->i_vertex_idxs[0] = index_data[dut->o_index_buff_addr].i0;
+        dut->i_vertex_idxs[1] = index_data[dut->o_index_buff_addr].i1;
+        dut->i_vertex_idxs[2] = index_data[dut->o_index_buff_addr].i2;
+    } else {
+        dut->i_vertex_idxs[0] = 0;
+        dut->i_vertex_idxs[1] = 0;
+        dut->i_vertex_idxs[2] = 0;
+    }
+}
+
+void assign_input_vertex(Vprimitive_assembler* dut) {
+    if (dut->o_vertex_read_en) {
+        if (dut->o_vertex_addr[0] >= vertex_pixel_data_size || dut->o_vertex_addr[1] >= vertex_pixel_data_size || dut->o_vertex_addr[2] >= vertex_pixel_data_size) {
+            return;
+        }
+
+        dut->i_v0[0] = truncate(vertex_pixel_data[dut->o_vertex_addr[0]].x, IV_DATAWIDTH);
+        dut->i_v0[1] = truncate(vertex_pixel_data[dut->o_vertex_addr[0]].y, IV_DATAWIDTH);
+        dut->i_v0_z = FixedPoint<int32_t>::fromFloat(vertex_pixel_data[dut->o_vertex_addr[0]].z, O_DEPTH_FRACBITS, O_DEPTH_FRACBITS+1).get();
+        dut->i_v0_invalid = vertex_pixel_data[dut->o_vertex_addr[0]].invalid;
+
+        dut->i_v1[0] = truncate(vertex_pixel_data[dut->o_vertex_addr[1]].x, IV_DATAWIDTH);
+        dut->i_v1[1] = truncate(vertex_pixel_data[dut->o_vertex_addr[1]].y, IV_DATAWIDTH);
+        dut->i_v1_z = FixedPoint<int32_t>::fromFloat(vertex_pixel_data[dut->o_vertex_addr[1]].z, O_DEPTH_FRACBITS, O_DEPTH_FRACBITS+1).get();
+        dut->i_v1_invalid = vertex_pixel_data[dut->o_vertex_addr[1]].invalid;
+
+        dut->i_v2[0] = truncate(vertex_pixel_data[dut->o_vertex_addr[2]].x, IV_DATAWIDTH);
+        dut->i_v2[1] = truncate(vertex_pixel_data[dut->o_vertex_addr[2]].y, IV_DATAWIDTH);
+        dut->i_v2_z = FixedPoint<int32_t>::fromFloat(vertex_pixel_data[dut->o_vertex_addr[2]].z, O_DEPTH_FRACBITS, O_DEPTH_FRACBITS+1).get();
+        dut->i_v2_invalid = vertex_pixel_data[dut->o_vertex_addr[2]].invalid;
+    } else {
+        dut->i_v0[0] = 0;
+        dut->i_v0[1] = 0;
+        dut->i_v0_z = 0;
+
+        dut->i_v1[0] = 0;
+        dut->i_v1[1] = 0;
+        dut->i_v1_z = 0;
+
+        dut->i_v2[0] = 0;
+        dut->i_v2[1] = 0;
+        dut->i_v2_z = 0;
+    }
+}
+
+int main(int argc, char** argv) {
+    srand(time(NULL));
+
+    Verilated::commandArgs(argc, argv);
+    Vprimitive_assembler* dut = new Vprimitive_assembler;
+    
+    Verilated::traceEverOn(true);
+    VerilatedVcdC* m_trace = new VerilatedVcdC;
+    dut->trace(m_trace, 5);
+    m_trace->open("waveform.vcd");
+    
+    for (int i = 0; i < RESET_CLKS; i++) {
+        dut->clk ^= 1;
+        dut->eval();
+    
+        dut->start = 0;
+
+        dut->i_vertex_idxs[0] = 0;
+        dut->i_vertex_idxs[1] = 0;
+        dut->i_vertex_idxs[2] = 0;
+
+        dut->i_v0[0] = 0;
+        dut->i_v0[1] = 0;
+        dut->i_v0_z = 0;
+
+        dut->i_v1[0] = 0;
+        dut->i_v1[1] = 0;
+        dut->i_v1_z = 0;
+
+        dut->i_v2[0] = 0;
+        dut->i_v2[1] = 0;
+        dut->i_v2_z = 0;
+
+        dut->rstn = 0;
+    
+        m_trace->dump(sim_time);
+        sim_time++;
+    }
+    dut->rstn = 1;
+    
+    while (sim_time < MAX_SIM_TIME) {
+        dut->clk ^= 1;
+        dut->eval();
+    
+        if (dut->clk == 1) {
+            posedge_cnt++;
+            dut->start = 0;
+
+            assign_input_index(dut);
+            assign_input_vertex(dut);
+
+            if (dut->ready) {
+                dut->i_num_triangles = index_data_size;
+                dut->start = 1;
+            }
+
+            if (dut->o_dv) {
+                printf("Triangle: \n");
+                printf("Vertex 0: (%d, %d, %f)\n", dut->o_vertex_pixel[0][0], dut->o_vertex_pixel[0][1], FixedPoint<int32_t>(dut->o_vertex_z[0], O_DEPTH_FRACBITS, O_DEPTH_FRACBITS + 1).toFloat());
+                printf("Vertex 1: (%d, %d, %f)\n", dut->o_vertex_pixel[1][0], dut->o_vertex_pixel[1][1], FixedPoint<int32_t>(dut->o_vertex_z[1], O_DEPTH_FRACBITS, O_DEPTH_FRACBITS + 1).toFloat());
+                printf("Vertex 2: (%d, %d, %f)\n", dut->o_vertex_pixel[2][0], dut->o_vertex_pixel[2][1], FixedPoint<int32_t>(dut->o_vertex_z[2], O_DEPTH_FRACBITS, O_DEPTH_FRACBITS + 1).toFloat());
+
+                printf("Bounding Box:\t");
+                printf("X: %d - %d\t", dut->bb_tl[0], dut->bb_br[0]);
+                printf("Y: %d - %d\n", dut->bb_tl[1], dut->bb_br[1]);
+
+                printf("\n");
+            }
+
+            if (dut->finished) {
+                printf("Finished\n");
+                break;
+            }
+        }
+    
+        m_trace->dump(sim_time);
+        sim_time++;
+    }
+    
+    m_trace->close();
+    delete dut;
+    exit(EXIT_SUCCESS);
+}
