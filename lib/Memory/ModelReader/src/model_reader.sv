b9c109398c70be5c8e074a4e24722c5bc376f3a4
Andreas V. Jonsterhaug <andreas.vj@icloud.com>
2024-11-15 14:25:22 +0100

    hopefully working model reader

--- lib/Memory/ModelReader/src/model_reader.sv
+++ lib/Memory/ModelReader/src/model_reader.sv
@@ -3,17 +3,18 @@
 module model_reader #(
     parameter integer MODEL_INDEX_WIDTH = 4,
     parameter integer HEADER_ADDR_WIDTH = 4,
-    parameter integer FACE_ADDR_WIDTH = 16,
-    parameter integer FACE_DATA_WIDTH = 36,  // 3 x 12 bits per face
-    parameter integer VERTEX_ADDR_WIDTH = 16,
-    parameter integer VERTEX_DATA_WIDTH = 48  // 3 x 16 bits per vertex
+    parameter integer FACE_ADDR_WIDTH = 12,
+    parameter integer VERTEX_ADDR_WIDTH = 12,
+    parameter integer COORD_WIDTH = 16,
+    parameter integer VERTEX_DATA_WIDTH = COORD_WIDTH * 3,
+    parameter integer FACE_DATA_WIDTH = FACE_ADDR_WIDTH * 3,
     parameter integer HEADER_DATA_WIDTH = FACE_ADDR_WIDTH + VERTEX_ADDR_WIDTH,
-    parameter string  HEADERS_FILE = "headers.mem"
-    parameter string  FACES_FILE = "faces.mem"
-    parameter string  VERTICES_FILE = "vertices.mem"
+    parameter string  HEADERS_FILE = "../../ModelReader/src/headers.mem",
+    parameter string  FACES_FILE = "../../ModelReader/src/faces.mem",
+    parameter string  VERTICES_FILE = "../../ModelReader/src/vertices.mem"
 )(
     input  logic                             clk,
-    input  logic                             reset,
+    input  logic                             rstn,
 
     input  logic [MODEL_INDEX_WIDTH-1:0]     model_index,
 
@@ -24,39 +25,36 @@ module model_reader #(
     output logic [VERTEX_DATA_WIDTH-1:0]     vertex_data,
 
     output logic                             face_buffer_done,
-    output logic                             vertex_buffer_done
+    output logic                             vertex_buffer_done,
+
+    output logic                             data_valid
 );
 
 logic  [HEADER_ADDR_WIDTH-1:0] header_addr;
 logic [HEADER_DATA_WIDTH-1:0]  header_data;
 
-logic  [FACE_ADDR_WIDTH-1:0]   face_addr;
-logic [FACE_DATA_WIDTH-1:0]    face_rom_data;
 
+logic  [FACE_ADDR_WIDTH-1:0]   face_addr;
 logic  [VERTEX_ADDR_WIDTH-1:0] vertex_addr;
-logic [VERTEX_DATA_WIDTH-1:0]  vertex_rom_data;
 
 // Start and end indices for faces and vertices
-logic [FACE_ADDR_WIDTH-1:0]    face_start_index;
 logic [FACE_ADDR_WIDTH-1:0]    face_end_index;
-
-logic [VERTEX_ADDR_WIDTH-1:0]  vertex_start_index;
 logic [VERTEX_ADDR_WIDTH-1:0]  vertex_end_index;
 
 // State machine states
-typedef enum logic [1:0] {
+typedef enum logic [2:0] {
     IDLE,
     READ_HEADER_0,
+    WAIT_HEADER_READ,
     READ_HEADER_1,
     READY
 } state_t;
-
-state_t state;
+state_t current_state, next_state;
 
 // Header ROM
 rom #(
-    .WIDTH(1 << HEADER_ADDR_WIDTH), // TODO set to actual size
-    .DEPTH(HEADER_DATA_WIDTH),
+    .WIDTH(HEADER_DATA_WIDTH),
+    .DEPTH(1 << HEADER_ADDR_WIDTH),  // TODO set to actual size
     .FILE(HEADERS_FILE)
 ) headers_rom (
     .clk(clk),
@@ -66,44 +64,66 @@ rom #(
 
 // Faces ROM
 rom #(
-    .WIDTH(1 << FACE_ADDR_WIDTH), // TODO set to actual size
-    .DEPTH(FACE_DATA_WIDTH),
+    .WIDTH(FACE_DATA_WIDTH),
+    .DEPTH(1 << FACE_ADDR_WIDTH),  // TODO set to actual size
     .FILE(FACES_FILE)
 ) faces_rom (
     .clk(clk),
     .addr(face_addr),
-    .data(face_rom_data)
+    .data(face_data)
 );
 
 // Vertices ROM
 rom #(
-    .WIDTH(1 << VERTEX_ADDR_WIDTH), // TODO set to actual size
-    .DEPTH(VERTEX_DATA_WIDTH),
+    .WIDTH(VERTEX_DATA_WIDTH),
+    .DEPTH(1 << VERTEX_ADDR_WIDTH), // TODO set to actual size
     .FILE(VERTICES_FILE)
 ) vertices_rom (
     .clk(clk),
     .addr(vertex_addr),
-    .data(vertex_rom_data)
+    .data(vertex_data)
 );
+
+
+always_ff @(posedge clk) begin
+    if (~rstn) begin
+        current_state <= IDLE;
+    end
+    else begin
+        current_state <= next_state;
+    end
+end
 
 // State transitions
 always_comb begin
-    next_state = current_state;
+    data_valid = 1'b0;
+    face_buffer_done = 1'b0;
+    vertex_buffer_done = 1'b0;
+
     case (current_state)
         IDLE: begin
             next_state = READ_HEADER_0;
         end
+
         READ_HEADER_0: begin
+            next_state = WAIT_HEADER_READ;
+        end
+
+        WAIT_HEADER_READ: begin
             next_state = READ_HEADER_1;
         end
+
         READ_HEADER_1: begin
             next_state = READY;
         end
+
         READY: begin
+            next_state = READY;
+            data_valid = 1'b1;
             if (face_addr == face_end_index - 1) face_buffer_done = 1'b1;
             if (vertex_addr == vertex_end_index - 1) vertex_buffer_done = 1'b1;
-            if (reset) next_state = IDLE;
         end
+
         default: begin
             next_state = IDLE;
         end
@@ -111,60 +131,47 @@ always_comb begin
 end
 
 // State operations
-always_ff @(posedge clk or posedge reset) begin
-    if (reset) begin
-        current_state <= IDLE;
-        face_addr <= {FACE_ADDR_WIDTH{1'b0}};
-        vertex_addr <= {VERTEX_ADDR_WIDTH{1'b0}};
-    end else begin
-        current_state <= next_state;
+always_ff @(posedge clk) begin
+    case (current_state)
+        IDLE: begin
+            header_addr <= model_index;
+        end
 
-        case (current_state)
-            IDLE: begin
-                // Reset counters and addresses
-                header_addr <= model_index;
-                face_buffer_done <= 1'b0;
-                vertex_buffer_done =< 1'b0;
+        READ_HEADER_0: begin
+            // Read start indices from header
+            face_addr <= header_data[HEADER_DATA_WIDTH-1:VERTEX_ADDR_WIDTH];
+            vertex_addr <= header_data[VERTEX_ADDR_WIDTH-1:0];
+            
+            // Increment header addr to read end indices
+            header_addr <= model_index + 1;
+        end
+
+        WAIT_HEADER_READ: begin
+            // Wait for header ROM to output data
+        end
+
+        READ_HEADER_1: begin
+            // Read end indices from header
+            face_end_index <= header_data[HEADER_DATA_WIDTH-1:VERTEX_ADDR_WIDTH];
+            vertex_end_index <= header_data[VERTEX_ADDR_WIDTH-1:0];
+        end
+
+        READY: begin
+            // Handle face data
+            if (next_face && !face_buffer_done) begin
+                face_addr <= (face_addr == face_end_index - 1) ? face_addr : face_addr + 1;
             end
 
-            READ_HEADER_0: begin
-                // Load start indices from header
-                face_start_index <= header_data[HEADER_DATA_WIDTH:VERTEX_ADDR_WIDTH];
-                vertex_start_index <= header_data[VERTEX_ADDR_WIDTH:0];
-                
-                // Read next header for end indices
-                header_addr <= model_index + 1;
+            // Handle vertex data
+            if (next_vertex && !vertex_buffer_done) begin
+                vertex_addr <= (vertex_addr == vertex_end_index - 1) ? vertex_addr : vertex_addr + 1;
             end
+        end
 
-            READ_HEADER_1: begin
-                // Load end indices from header
-                face_end_index <= header_data[HEADER_DATA_WIDTH:VERTEX_ADDR_WIDTH];
-                vertex_end_index <= header_data[VERTEX_ADDR_WIDTH:0];
-                
-                // Initialize face and vertex addresses
-                face_addr <= face_start_index;
-                vertex_addr <= vertex_start_index;
-            end
-
-            READY: begin
-                // Handle face data
-                if (next_face && !face_buffer_done) begin
-                    face_data <= face_rom_data;
-                    face_addr <= (face_addr == face_end_index - 1) ? face_addr : face_addr + 1;
-                end
-
-                // Handle vertex data
-                if (next_vertex && !vertex_buffer_done) begin
-                    vertex_data <= vertex_rom_data;
-                    vertex_addr <= (vertex_addr == vertex_end_index - 1) ? vertex_addr : vertex_addr + 1;
-                end
-            end
-
-            default: begin
-                // Do nothing
-            end
-        endcase
-    end
+        default: begin
+            // Do nothing
+        end
+    endcase
 end
 
 endmodule
---
+++ lib/Memory/ModelReader/tb/tb_model_reader.cpp
@@ -0,0 +1,64 @@
+#include <stdlib.h>
+#include <iostream>
+#include <cstdlib>
+#include <verilated.h>
+#include <verilated_vcd_c.h>
+#include "obj_dir/Vmodel_reader.h"
+
+#define MAX_SIM_TIME 100
+
+vluint64_t sim_time = 0;
+vluint64_t posedge_cnt_write = 0;
+
+int main(int argc, char** argv) {
+    srand(time(NULL));
+    Verilated::commandArgs(argc, argv);
+
+    Vmodel_reader* dut = new Vmodel_reader;
+
+    Verilated::traceEverOn(true);
+    VerilatedVcdC* m_trace = new VerilatedVcdC;
+    dut->trace(m_trace, 5);
+    m_trace->open("waveform.vcd");
+        
+    dut->next_face = 1;
+    dut->next_vertex = 1;
+    dut->model_index = 1;
+
+    while (sim_time < MAX_SIM_TIME) { 
+        dut->clk ^= 1;
+        dut->eval(); 
+
+        if (sim_time < 4) {
+            dut->rstn = 0;
+        }
+        else if (43 <= sim_time && sim_time <= 45) {
+            dut->rstn = 0;
+            dut->model_index = 0;
+        }
+        else {
+            dut->rstn = 1;
+        }
+
+        if (16 < sim_time && sim_time < 20) {
+            dut->next_vertex = 0;
+            dut->next_face = 0;
+        }
+        else {
+            dut->next_vertex = 1;
+            dut->next_face = 1;
+        }
+
+        
+
+
+        m_trace->dump(sim_time);
+        sim_time++;
+    }
+
+    m_trace->close();
+
+    delete dut;
+
+    exit(EXIT_SUCCESS);
+}
